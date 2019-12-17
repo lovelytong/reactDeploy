c24035ca615ba0fe2f4c51ba894141eeb77f6d35
tongyu <27755953@qq.com>
2019-12-17 15:01:56 +0800

    first submit

---
+++ precache-manifest.19d1a4c4ed5c098f250654c47f05b20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1e9ea264e9123f43e89cf2fc25e8ad",
+    "url": "/reactDeploy/index.html"
+  },
+  {
+    "revision": "2b445b1984be474ca204",
+    "url": "/reactDeploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3da17989d5c5da24ec10",
+    "url": "/reactDeploy/static/js/2.47b8276d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactDeploy/static/js/2.47b8276d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b445b1984be474ca204",
+    "url": "/reactDeploy/static/js/main.be24346d.chunk.js"
+  },
+  {
+    "revision": "6b02cbbc3fefc0d5c1ee",
+    "url": "/reactDeploy/static/js/runtime-main.b02a675e.js"
+  }
+]);
